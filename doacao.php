2dd1c70d48b1c1d6509d08f4f92364ce8fe6f552
francisco-cruz <franaraujosilva2017@gmail.com>
2022-11-03 17:19:33 -0300

    tela de agradecimento/ tela de registros de doação

--- doacao.php
+++ doacao.php
@@ -23,38 +23,61 @@ if (isset($_POST['enviar'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/dashboard.css">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-    <title>Formulário</title>
+    <title>Doação</title>
 </head>
 
 <body>
-
+  <nav class="navbar navbar-expand-lg position-fixed top-0">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="./assets/img/logo medio.png" alt=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="./dashboard.html">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="./registroDoacao.html">Minhas Doações</a>
+          </li>
+  
+        </ul>
+        <a href="./doacao.html">
+          <button class="btn btn-secundario rounded-5">Quero Doar</button>
+        </a>
+      </div>
+    </div>
+  </nav>
     <form action="?" method="POST" class="card px-4 py-4">
-        <h3 class="text-center mb-5">Faça uma Doação</h3>
-        <select class="form-select rounded-5 mb-3" id="inputDoacao" name = "doacao">
+        <h3 class="text-center mb-5 cinza-primario">Faça uma Doação</h3>
+        <select class="form-select rounded-5 mb-3" id="inputDoacao">
             <option selected value="none">Tipo de Doação</option>
-            <option value="mantimentos">Mantimentos(Comida ou Remédios)</option>
+            <option value="mantimentos">Matimentos</option>
             <option value="dinheiro">Dinheiro</option>
         </select>
         <div class="mb-3 d-flex gap-3">
             <div>
-                <input placeholder="Quantidade" type="number" min="1" class="form-control rounded-5" id="inputQuantidade" name="qtd" disabled>
+                <input placeholder="Quantidade" type="number" min="1" class="form-control rounded-5"
+                    id="inputQuantidade" disabled>
             </div>
             <div>
-                <input placeholder="Valor" type="text" class="form-control rounded-5" id="inputValor" name="valor" disabled>
-
+                <input placeholder="Valor" type="text" class="form-control rounded-5" id="inputValor" disabled>
             </div>
         </div>
-        <button type="submit" name="enviar" class="btn btn-lg btn-warning rounded-5 mt-3">Doar</button>
-    </form>
+        <button type="submit" class="btn btn-lg btn-primario rounded-5 mt-3">Doar</button>
 
+    </form>
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
 
     <script src="./assets/scripts/doacao.js"></script>
 </body>
-
 </html>
